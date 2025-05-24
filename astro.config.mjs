8c618237f7a9d579631c4505829c0bd1af35cad9
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2025-05-25 02:34:54 +0500

    use stores

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import UnoCSS from "unocss/astro";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://the-keeper.github.io',
-  base: 'prime-harmony',
-  i18n: {
-    locales: ["ru", "en"],
-    defaultLocale: "en",
-    routing: {
-      prefixDefaultLocale: false,
-      fallbackType: "rewrite",
-    }
-  },
+    site: "https://the-keeper.github.io",
+    base: "prime-harmony",
+    i18n: {
+        locales: ["ru", "en"],
+        defaultLocale: "en",
+        routing: {
+            prefixDefaultLocale: false,
+            fallbackType: "rewrite",
+        },
+    },
+    integrations: [UnoCSS(), svelte()],
 });
---
+++ src/store.ts
@@ -0,0 +1,3 @@
+import { atom, map } from 'nanostores';
+
+export const quarterLayout = atom(10 as 5 | 6 | 9 | 10);
