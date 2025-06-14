a1fc28edc8070c0cd0049fac67b0c606a6b354ad
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2025-06-14 22:00:21 +0500

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import UnoCSS from "unocss/astro";
-import svelte from "@astrojs/svelte";
+// import UnoCSS from "unocss/astro";
 
 // https://astro.build/config
 export default defineConfig({
